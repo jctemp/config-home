9e766272bcc0b5e68ad4640355b39f5a4999d898
Jamie Temple <jamie-temple@live.de>
2024-02-23 18:05:21 +0100

    chore(project): moved home form system

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "NixOS home configuration";
+
+  nixConfig = {
+    experimental-features = ["nix-command" "flakes"];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {self, ...}:
+    inputs.flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      username = "temple";
+      stateVersion = "23.11";
+    in {
+      formatter = pkgs.alejandra;
+      packages.homeConfigurations = {
+        ${username} = inputs.home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          extraSpecialArgs = {
+            inherit username stateVersion;
+          };
+          modules = ["${self}/configuration.nix"];
+        };
+      };
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          (writeShellScriptBin "check" ''
+            nix fmt --no-write-lock-file
+            home-manager build --flake . --dry-run --print-build-logs
+          '')
+          (writeShellScriptBin "update" ''
+            nix fmt --no-write-lock-file
+            nix flake update --commit-lock-file
+          '')
+          (writeShellScriptBin "upgrade" ''
+            if [ -z "$1" ]; then
+              username=$(whoami)
+            else
+              username=$1
+            fi
+
+            nix fmt --no-write-lock-file
+            home-manager switch --flake .#''${username}
+          '')
+          alejandra
+          deadnix
+          nil
+          statix
+        ];
+      };
+    });
+}
