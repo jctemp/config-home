9b481d1c3d2a7f7e22f65d64bcbe121bb531d718
Jamie Temple <jamie-temple@live.de>
2024-04-24 07:21:07 +0200

    refactor(syncing): change rclone for syncthing

--- configuration.nix
+++ configuration.nix
@@ -8,82 +8,6 @@
     ./programs
   ];
 
-  # To write systemd services do not look at the nixpkgs. After service name
-  # simply replicate the systemd service file with the Nix language.
-  #
-  # https://www.freedesktop.org/software/systemd/man/latest/systemd.service.html
-  systemd.user = {
-    enable = true;
-    services = {
-      "rclone-mount" = {
-        Unit = {
-          Description = "Mount Google Drive";
-          After = ["network-online.target"];
-          Wants = ["network-online.target"];
-        };
-        Service = {
-          ExecStart = ''
-            ${pkgs.rclone}/bin/rclone mount \
-              --config /home/${username}/.config/rclone/rclone.conf \
-              --transfers 32 \
-              --allow-other \
-              drive: /home/${username}/Drive
-          '';
-          ExecStop = ''
-            ${pkgs.fuse}/bin/fusermount -u /home/${username}/Drive
-          '';
-          Restart = "always";
-          RestartSec = "5s";
-        };
-        Install = {
-          WantedBy = ["default.target"];
-        };
-      };
-      "rclone-sync" = {
-        Unit = {
-          Description = "Sync Google Drive";
-          After = ["network-online.target"];
-          Requires = ["rclone-mount.service"];
-        };
-        Service = {
-          Type = "oneshot";
-          ExecStart = ''
-            ${pkgs.rclone}/bin/rclone sync \
-              --fast-list \
-              --transfers 32 \
-              drive: /home/${username}/Drive
-          '';
-          Restart = "on-failure";
-          RestartSec = "5s";
-        };
-        Install = {
-          WantedBy = ["default.target"];
-        };
-      };
-      "rclone-backup-local" = {
-        Unit = {
-          Description = "Backup to local";
-          After = ["network-online.target"];
-          Requires = ["rclone-mount.service"];
-        };
-        Service = {
-          Type = "oneshot";
-          ExecStart = ''
-            ${pkgs.rclone}/bin/rclone copy \
-              --fast-list \
-              --transfers 32 \
-              drive: /home/${username}/Backup
-          '';
-          Restart = "on-failure";
-          RestartSec = "5s";
-        };
-        Install = {
-          WantedBy = ["default.target"];
-        };
-      };
-    };
-  };
-
   home = {
     inherit username stateVersion;
     homeDirectory = "/home/${username}";
@@ -102,6 +26,12 @@
       zotero
       rclone
     ];
+  };
+
+  services.syncthing = {
+    enable = true;
+    tray.enable = true;
+    extraOptions = [];
   };
 
   systemd.user.startServices = "sd-switch";
