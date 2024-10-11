f652ff568809f3a118376b9479e67a19734afd04
Jamie Temple <jamie-temple@live.de>
2024-10-11 23:54:33 +0200

    feat(helix): add configuration file LSP

--- modules/dev-tools.nix
+++ modules/dev-tools.nix
@@ -43,6 +43,34 @@
         languages = {
           language-server = {
             nixd = {command = "${pkgs.nixd}/bin/nixd";};
+            yaml-language-server = {
+              command = "${pkgs.yaml-language-server}/bin/yaml-language-server";
+              args = ["--stdio"];
+            };
+            jsonnet-language-server = {
+              command = "${pkgs.jsonnet-language-server}/bin/jsonnet-language-server";
+              args = ["-t" "--lint"];
+            };
+            taplo = {
+              command = "${pkgs.taplo}/bin/taplo";
+              args = ["lsp" "stdio"];
+            };
+            bash-language-server = {
+              command = "${pkgs.nodePackages.bash-language-server}/bin/bash-language-server";
+              args = ["start"];
+            };
+            marksman = {
+              command = "${pkgs.marksman}/bin/marksman";
+              args = ["server"];
+            };
+            docker-langserver = {
+              command = "${pkgs.dockerfile-language-server-nodejs}/bin/docker-langserver";
+              args = ["--stdio"];
+            };
+            docker-compose-langserver = {
+              command = "${pkgs.docker-compose-language-service}/bin/docker-compose-langserver";
+              args = ["--stdio"];
+            };
           };
           language = [
             {
